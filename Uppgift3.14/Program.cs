a174f17c9bbbde59061a85be8750ec35a0880fd4
Mahdi <mhdisy0@gmail.com>
2022-09-30 10:40:10 +0200

    Add project files.

---
+++ Uppgift3.14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace uppgift3_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har då gåt genom gymnasiet? Svara J för Ja eller N för Nej");
+            string svar = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (svar.ToLower() == "j" && ålder <= 22)
+                Console.WriteLine("Vi vill gärna inställa dig");
+            else Console.Write("Vi letar tyvärr efter annan personal just nu.");
+        }
+    }
+}
